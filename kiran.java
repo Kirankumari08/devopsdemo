a09badcbdd3f1f624fc83950f76a04e4f73b6e9f
Kirankumari08 <kiranbhagat.ptu@gmail.com>
2024-10-17 00:48:26 +0530

    adding new changes

--- kiran.java
+++ kiran.java
@@ -1,1 +1,9 @@
-hi kiran how are you.
+Hi my name is kiran and I am learning devops.
+Also docker and kubernatives 
+I am taking classes daily for one hr
+ALso under guidence of my Husband
+
+
+
+
+
