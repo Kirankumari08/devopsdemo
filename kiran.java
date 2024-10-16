c5b9d6d7a4bb1675c2d66556be6ae622b001f542
Kirankumari08 <36217637+Kirankumari08@users.noreply.github.com>
2024-10-17 01:09:32 +0530

    making changes (#5)

--- kiran.java
+++ kiran.java
@@ -1,9 +1,7 @@
+
+
+my name is kiran
+
 Hi my name is kiran and I am learning devops.
 Also docker and kubernatives 
 I am taking classes daily for one hr
-ALso under guidence of my Husband
-
-
-
-
-
